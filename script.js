ccf07b9a6b2a284277269d57594b4810f68844ef
Pankaj Mokashi <127533657+pankajmokashi@users.noreply.github.com>
2023-04-18 16:01:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const timer = document.getElementById("timer")
+
+function display(){
+    var x = new Date()
+
+    var ampm = x.getHours( ) >= 12 ? ' PM' : ' AM';
+
+    hours = x.getHours( ) % 12;
+    hours = hours ? hours : 12;
+
+    var minutes=x.getMinutes().toString()
+    minutes=minutes.length==1 ? 0+minutes : minutes;
+
+    var seconds=x.getSeconds().toString()
+    seconds=seconds.length==1 ? 0+seconds : seconds;
+
+    var datetime = x.getMonth() +1 + "/" + x.getDate() + "/" + x.getFullYear();
+    datetime = datetime + ", " + hours + ":" + minutes + ":" + seconds
+    timer.innerText = datetime + " " + ampm
+    display_ref()
+}
+
+function display_ref()
+{
+    setTimeout('display()', 1000)
+}
+display_ref()
